f98207a334a11ac65646094bbb98bbc799b40145
mnapo <xnapohx@gmail.com>
2019-09-16 15:30:27 -0300

    versión 1

--- items_by_seller-id.py
+++ items_by_seller-id.py
@@ -16,24 +16,25 @@ url_api_items = url_api + "/items?ids="
 
 seller_id = "81644614"
 log_content = ""
+delimiter = ","
 
 ##################################
 
 
 # se obtienen los items del seller_id=81644614
-req = requests.get( url_api_MLA_seller_id + seller_id )
+seller_req = requests.get( url_api_MLA_seller_id + seller_id )
 
 # se deserializa la estructura JSON a diccionario
 req_json = req.json()
 
 # se obtiene la lista de IDs de los ítems publicados por el vendedor
-results = req[ 'results' ]
+results = req_json[ "results" ]
 
-# se transforma el diccionario a cadena separada por comas, para pasar mediante GET a la API
-items_list = diccionario_a_cadena( results )
+# se convierte la lista a cadena separada por comas, para pasar mediante GET a la API
+items_list = delimiter.join( results )
 
 # se obtiene la info de cada ítem publicado
-req = requests.get( url_api_items + items_list )
+items_req = requests.get( url_api_items + items_list )
 
 
 # se parsea la lista de IDs de ítems como cadena 
